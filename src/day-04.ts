765ba891e71b6dfa58c73e234bd759046d901bcb
airfortech <airm@xxx.yy>
2022-12-04 08:25:37 +0100

    day-04

--- src/day-04.ts
+++ src/day-04.ts
@@ -5,10 +5,46 @@ import { title } from "./utils/title";
 import "./utils/Array";
 
 // part 1
-const part1 = async (data: string) => {};
+const part1 = async (data: string) => {
+  return data
+    .split("\r\n")
+    .map(section =>
+      section
+        .split(",")
+        .map(a => a.split("-"))
+        .map(a => a.toNumbers())
+        .map(a => new Array(a[1] - a[0] + 1).fill(1).map((_, i) => a[0] + i))
+    )
+    .map(a => {
+      return (
+        a[0].every(b => a[1].includes(b)) || a[1].every(b => a[0].includes(b))
+      );
+    })
+    .filter(a => a === true).length;
+};
 
 // part 2
-const part2 = async (data: string) => {};
+const part2 = async (data: string) => {
+  return data
+    .split("\r\n")
+    .map(section =>
+      section
+        .split(",")
+        .map(a => a.split("-"))
+        .map(a => a.toNumbers())
+        .map(a => [
+          ...Array(a[1] - a[0] + 1)
+            .fill(1)
+            .map((_, i) => a[0] + i),
+        ])
+    )
+    .map(a => {
+      return (
+        a[0].some(b => a[1].includes(b)) || a[1].some(b => a[0].includes(b))
+      );
+    })
+    .filter(a => a === true).length;
+};
 
 (async () => {
   const data = await getData("input-day-04.txt");
