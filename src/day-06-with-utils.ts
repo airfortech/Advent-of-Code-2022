4d9aa613551e5e5b5a706250291ec77a6c97c4b9
airfortech <airm@xxx.yy>
2022-12-06 13:14:50 +0100

    day-06-with-utils

---
+++ src/day-06-with-utils.ts
@@ -0,0 +1,34 @@
+// https://adventofcode.com/2022/day/6
+
+import { getData } from "./utils/getData";
+import { title } from "./utils/title";
+import "./utils/String";
+
+const findMarker = (string: string, length: number): number => {
+  for (let i = 0; i < string.length; i++) {
+    const word = string.slice(i, i + length);
+    if (!word.hasDuplicates()) return i + length;
+  }
+};
+
+// part 1
+const part1 = async (data: string) => {
+  return findMarker(data, 4);
+};
+
+// part 2
+const part2 = async (data: string) => {
+  return findMarker(data, 14);
+};
+
+(async () => {
+  const data = await getData("input-day-06.txt");
+
+  title("day 6");
+
+  console.log("part 1");
+  console.log(await part1(data));
+
+  console.log("\npart 2");
+  console.log(await part2(data));
+})();
